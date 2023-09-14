128c74d494d925e9b75bbb83cd86a84594c626c2
jessvburton <93324273+jessvburton@users.noreply.github.com>
2023-09-14 09:15:34 +0100

    Create sheets.py

---
+++ GoogleSheet_Extraction/GCP_Execution/sheets.py
@@ -0,0 +1,50 @@
+SHEET_LIST = [  # You can add more Google Sheets here, just keep the same formatting.
+    {  # You can put the worksheet name here as a reminder
+        'id': '', # This is the Google Sheet ID which can be found in the URL of the Google Sheet
+        'worksheets': ['Original worksheet name 1', 'Original worksheet_name 4'], # Make sure the worksheets are in exactly the same format, from spacing to characters, otherwise it won't recognise them
+        'worksheet_rename_mapping': { # This renaming will not rename the original worksheet names, but the copy you are creating
+            'Original worksheet name 1': 'New worksheet name 1', 
+            'Original worksheet name 4': 'New worksheet name 4'
+        },
+        'column_rename_mapping': { # This renaming will not rename the original worksheet columns, but the copy you are creating
+            0: 'Column 1', # This renamining works on column index position
+            1: 'Column 2',
+            2: 'Column 3',
+            3: 'Column 4',
+            4: 'Column 5',
+            5: 'Column 6',
+            6: 'Column 7',
+            7: 'Column 8',
+            8: 'Column 9',
+            9: 'Column 10'
+        },
+        'bucket_name': 'data-analytics', # Example naming
+        'folder_root': 'google-sheets-as-csvs/' # Example naming
+    },
+    {  # Here is a bank version
+       # You can add as many worksheet names or columns as you wish. 
+       # Keep repeating to add more Google Sheets, or to rename worksheets/columns differently for the same Google Sheet.
+        'id': '',
+        'worksheets': [", ""],
+        'worksheet_rename_mapping': {
+            "": "",
+            "": "",
+            "": "",
+            "": ""
+        },
+        'column_rename_mapping': {
+            0: '',
+            1: '',
+            2: '',
+            3: '',
+            4: '',
+            5: '',
+            6: '',
+            7: '',
+            8: '',
+            9: ''
+        },
+        'bucket_name': '',
+        'folder_root': ''
+    }
+]
